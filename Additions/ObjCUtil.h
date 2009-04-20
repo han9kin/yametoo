493729c6c8f7295a1dbce7317bdcde9661ace678
Sanghyuk Suh <han9kin@gmail.com>
2009-04-20 06:51:17 +0000

    SYNTHESIZE_SINGLETON_CLASS_WITH_RETURNTYPE

--- Additions/ObjCUtil.h
+++ Additions/ObjCUtil.h
@@ -10,57 +10,60 @@
 
 #pragma mark making singleton class
 
-#define SYNTHESIZE_SINGLETON_CLASS(aClassName, aAccessor)       \
-                                                                \
-    static aClassName *aAccessor = nil;                         \
-                                                                \
-+ (aClassName *)aAccessor                                       \
-{                                                               \
-    @synchronized(self)                                         \
-    {                                                           \
-        if (!aAccessor)                                         \
-        {                                                       \
-            [[self alloc] init];                                \
-        }                                                       \
-    }                                                           \
-                                                                \
-    return aAccessor;                                           \
-}                                                               \
-                                                                \
-+ (id)allocWithZone:(NSZone *)aZone                             \
-{                                                               \
-    @synchronized(self)                                         \
-    {                                                           \
-        if (!aAccessor)                                         \
-        {                                                       \
-            aAccessor = [super allocWithZone:aZone];            \
-            return aAccessor;                                   \
-        }                                                       \
-    }                                                           \
-                                                                \
-    return nil;                                                 \
-}                                                               \
-                                                                \
-- (id)copyWithZone:(NSZone *)aZone                              \
-{                                                               \
-    return self;                                                \
-}                                                               \
-                                                                \
-- (id)retain                                                    \
-{                                                               \
-    return self;                                                \
-}                                                               \
-                                                                \
-- (NSUInteger)retainCount                                       \
-{                                                               \
-    return NSUIntegerMax;                                       \
-}                                                               \
-                                                                \
-- (void)release                                                 \
-{                                                               \
-}                                                               \
-                                                                \
-- (id)autorelease                                               \
-{                                                               \
-    return self;                                                \
+
+#define SYNTHESIZE_SINGLETON_CLASS(aClassName, aAccessor) SYNTHESIZE_SINGLETON_CLASS_WITH_RETURNTYPE(aClassName, aClassName *, aAccessor)
+
+#define SYNTHESIZE_SINGLETON_CLASS_WITH_RETURNTYPE(aClassName, aReturnType, aAccessor)  \
+                                                                                        \
+    static aClassName *aAccessor = nil;                                                 \
+                                                                                        \
++ (aReturnType)aAccessor                                                                \
+{                                                                                       \
+    @synchronized(self)                                                                 \
+    {                                                                                   \
+        if (!aAccessor)                                                                 \
+        {                                                                               \
+            [[self alloc] init];                                                        \
+        }                                                                               \
+    }                                                                                   \
+                                                                                        \
+    return aAccessor;                                                                   \
+}                                                                                       \
+                                                                                        \
++ (id)allocWithZone:(NSZone *)aZone                                                     \
+{                                                                                       \
+    @synchronized(self)                                                                 \
+    {                                                                                   \
+        if (!aAccessor)                                                                 \
+        {                                                                               \
+            aAccessor = [super allocWithZone:aZone];                                    \
+            return aAccessor;                                                           \
+        }                                                                               \
+    }                                                                                   \
+                                                                                        \
+    return nil;                                                                         \
+}                                                                                       \
+                                                                                        \
+- (id)copyWithZone:(NSZone *)aZone                                                      \
+{                                                                                       \
+    return self;                                                                        \
+}                                                                                       \
+                                                                                        \
+- (id)retain                                                                            \
+{                                                                                       \
+    return self;                                                                        \
+}                                                                                       \
+                                                                                        \
+- (NSUInteger)retainCount                                                               \
+{                                                                                       \
+    return NSUIntegerMax;                                                               \
+}                                                                                       \
+                                                                                        \
+- (void)release                                                                         \
+{                                                                                       \
+}                                                                                       \
+                                                                                        \
+- (id)autorelease                                                                       \
+{                                                                                       \
+    return self;                                                                        \
 }
